6e1f0c4fe5d99b20fdfe5c9426c18849b8fda241
Madalyn Marino <112964220+madalynmm@users.noreply.github.com>
2023-02-27 09:19:04 -0500

    added functionality to lib files and updated html output

--- src/page-template.js
+++ src/page-template.js
@@ -22,12 +22,40 @@ const generateTeam = team => {
 
   // create the html for engineers
   const generateEngineer = engineer => {
-      //code goes here
+    return `
+      <div class="card employee-card">
+      <div class="card-header">
+          <h2 class="card-title">${engineer.getName()}</h2>
+          <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+      </div>
+      <div class="card-body">
+          <ul class="list-group">
+              <li class="list-group-item">ID: ${engineer.getId()}</li>
+              <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+              <li class="list-group-item">GitHub Profile: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+          </ul>
+      </div>
+  </div>
+      `;
   };
 
   // create the html for interns
   const generateIntern = intern => {
-      //code goes here
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+    `;
   };
 
   const html = [];
